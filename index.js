24b5812a6c071b145312435acf4e5a934e5f276f
Joseph-kirigo <josephkirigo777@gmail.com>
2022-08-20 12:34:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('watch football');
+
+
+function mondayWork(activity= 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork('write a newsletter')
+
+
+function wrapAdjective(visualFlair = "*"){
+    return function(adjective = "special"){
+      return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+  }
+  wrapAdjective("*")("a dedicated programmer")
